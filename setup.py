11f671cc853ca1ea829566131578106ad53ce396
Keiron O'Shea <keiron.oshea@gmail.com>
2017-09-05 13:40:50 +0100

    Fixed the setup file

--- setup.py
+++ setup.py
@@ -2,14 +2,11 @@ from distutils.core import setup
 
 setup(
     name='dimedbpy',
-    version='0.0.1',
-    packages=['pyidick'],
-    url='https://www.github.com/dimedbpy',
+    version='0.0.1a',
+    packages=['dimedbpy'],
+    url='https://www.github.com/KeironO/dimedbpy',
     license='MIT',
     author='Keiron OShea',
     author_email = 'keo7@aber.ac.uk',
-    description = 'Python wrapper for DIMEdbs REST API',
-    package_data={
-      'pyidick': ['data/*.json'],
-   },
+    description = 'Python wrapper for DIMEdb\'s REST API',
 )
