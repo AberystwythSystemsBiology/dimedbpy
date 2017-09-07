10470533bccc1f37866e9e5aaee17568755f8c69
Keiron O'Shea <keiron.oshea@gmail.com>
2017-09-07 16:48:37 +0100

    New setup file

--- setup.py
+++ setup.py
@@ -1,13 +1,24 @@
 from distutils.core import setup
+from pip.req import parse_requirements
 
 setup(
     name='dimedbpy',
-    version='0.0.1a',
+    version='0.0.2',
     packages=['dimedbpy'],
     url='https://www.github.com/KeironO/dimedbpy',
     license='MIT',
     author='Keiron OShea',
     author_email = 'keo7@aber.ac.uk',
     description = 'Python wrapper for DIMEdb\'s REST API',
-    install_requires=['requests']
+    install_requires=["pandas", "requests"],
+    classifiers=[
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Development Status :: 2 - Pre-Alpha',
+        'Topic :: Scientific/Engineering :: Bio-Informatics',
+        'License :: OSI Approved :: MIT License',
+        'Intended Audience :: Science/Research',
+        'Intended Audience :: Developers'
+    ]
 )
